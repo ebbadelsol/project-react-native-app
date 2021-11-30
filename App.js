3afc8af4271d7c1a434e9cc2adefee5e2efc68bb
e-delsol <80672920+e-delsol@users.noreply.github.com>
2021-11-30 14:14:17 +0100

    Added a home, about, random quote screen and more. Setted up my magic ball function within the drawer navigation

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { Feed } from './screens/Feed';
-import { Notifications } from './screens/Notifications';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { RandomQuote } from "./screens/RandomQuote";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { About } from "./screens/About";
+import { Home } from "./screens/Home";
 
 const Drawer = createDrawerNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Feed" component={Feed} />
-        <Drawer.Screen name="Notifications" component={Notifications} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Drawer.Navigator>
+				<Drawer.Screen name="Home" component={Home} />
+				<Drawer.Screen name="About" component={About} />
+				<Drawer.Screen name="Random Quote" component={RandomQuote} />
+			</Drawer.Navigator>
+		</NavigationContainer>
+	);
 };
 
 export default App;
